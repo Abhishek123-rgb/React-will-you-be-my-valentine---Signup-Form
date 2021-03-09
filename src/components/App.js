e8c66b74f560b3ba3e8c79ba69187fb4d02d4250
Abhishek123-rgb <71871455+Abhishek123-rgb@users.noreply.github.com>
2021-03-09 15:40:46 +0530

    form completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,112 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 const App = () => {
-  return (
-    <div id="main"></div>
-  )
-}
+  const [nameMessage, setNameMessage] = useState("");
+  const [emailMessage, setEmailMessage] = useState("");
+  const [phoneMessage, setPhoneMessage] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
+  const [error, setError] = useState("");
+  const [congrats, setCongrats] = useState("");
 
+  const validation = (e) => {
+    e.preventDefault();
+    const Name = document.querySelector("input[data-testid=name]").value;
+    const email = document.querySelector("input[data-testid=email]").value;
+    const phone = document.querySelector("input[data-testid=phoneNumber]")
+      .value;
+    const password = document.querySelector("input[data-testid=password]")
+      .value;
+
+    if (Name === "" || phone === "" || password === "" || email === "") {
+      setError("All fields are mandatory");
+      setCongrats("");
+      setNameMessage("");
+      setEmailMessage("");
+      setPasswordMessage("");
+    }
+    else if (/[^0-9a-zA-Z]/.test(Name)) {
+      setNameMessage("Name is not alphanumeric");
+      setCongrats("");
+      setError("");
+      setEmailMessage("");
+      setPasswordMessage("");
+    } else if (email.indexOf("@") === -1) {
+      setEmailMessage("Email must contain @");
+      setCongrats("");
+      setError("");
+      setNameMessage("");
+      setPasswordMessage("");
+    } else if (isNaN(phone)) {
+      setPhoneMessage("Phone Number must contain only numbers");
+      setCongrats("");
+      setError("");
+      setNameMessage("");
+      setEmailMessage("");
+      setPasswordMessage("");
+    } else if (password.length <= 6) {
+      setPasswordMessage("Password must contain atleast 6 letters");
+      setCongrats("");
+      setError("");
+      setNameMessage("");
+      setEmailMessage("");
+      setPhoneMessage("");
+    } else {
+      let userName = email.slice(0, email.indexOf("@"));
+      setCongrats(`Hello ${userName}`);
+      setError("");
+      setNameMessage("");
+      setEmailMessage("");
+      setPhoneMessage("");
+      setPasswordMessage("");
+    }
+  };
+  return (
+    <>
+      <form onSubmit={validation}>
+        <div>
+          <label>Name: </label>
+          <input type="text" data-testid="name" />
+          <div>{nameMessage}</div>
+        </div>
+        <br />
+        <div>
+          <label>Email address :</label>
+          <input type="text" data-testid="email" />
+          <div>{emailMessage}</div>
+        </div>
+        <br />
+        <div>
+          <label>Gender :</label>
+          <select data-testid="gender">
+            <option value="male" selected>
+              Male
+            </option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <br />
+        <div>
+          <label>Phone Number :</label>
+          <input type="text" data-testid="phoneNumber" />
+          <div>{phoneMessage}</div>
+        </div>
+        <br />
+        <div>
+          <label>Password :</label>
+          <input type="password" data-testid="password" />
+          <div>{passwordMessage}</div>
+        </div>
+        <br />
+        <div>
+          <input type="submit" data-testid="submit" />
+        </div>
+        <div>{error}</div>
+        <div>{congrats}</div>
+      </form>
+    </>
+  );
+};
 
 export default App;
